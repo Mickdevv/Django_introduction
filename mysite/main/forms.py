fffe860c8b6ea39cd2c758b6608cd269044c34d1
Michael <michael@devereux.fm>
2023-09-21 17:17:49 +0200

    Form and list display beautification

--- mysite/main/forms.py
+++ mysite/main/forms.py
@@ -3,8 +3,8 @@ from django import forms
 class CreateNewList(forms.Form):
     name = forms.CharField(label="Name", max_length=200, required=True)
     check = forms.BooleanField(label="Available", required=False)
-    age = forms.IntegerField(label="Age")
+    age = forms.IntegerField(label="Age", min_value=0, max_value=151)
 
 class CreateNewitem(forms.Form):
     name = forms.CharField(label="Name", required=False)
-    age = forms.IntegerField(label="Age", max_value=151)
+    age = forms.IntegerField(label="Age", min_value=0, max_value=151)
